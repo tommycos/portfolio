df021ad2f2fd858bb6e4e31976414e47ba733aa8
tommycos <tomcos.2014@gmail.com>
2022-04-04 16:58:54 +0200

    changed about me
    added photo and sample text to about me page, turned it into it's own component instead of it being a paragraph in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./navbar/Navbar";
+import About from "./about/About";
 
 export default function App() {
   const [navClick, setNavClick] = useState(0);
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <main>
       <Navbar onClick={changeNav} currentNav={navClick} />
-      {navClick === 0 && <p>This is my ABOUT ME</p>}
+      {navClick === 0 && <About />}
       {navClick === 1 && <p>This is my CV</p>}
       {navClick === 2 && <p>These are my Projects</p>}
     </main>
